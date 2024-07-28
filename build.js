317a036851918d19906880242c5ed154eacfe159
Oluwaseyi <hassanabdullahioluwaseyi@gmail.com>
2024-07-28 15:13:30 +0100

    new commit

--- build.js
+++ build.js
@@ -1,14 +1,16 @@
 import fs from 'fs';
 import transfers from './transfers.js';
 
-(async () => {
+const trans = async () => {
     try {
         console.log('Starting data fetch...');
-        const data = await transfers();
+        const data = await transfers(2);
         console.log('Data fetched:', data);
         fs.writeFileSync('transfers.json', JSON.stringify(data, null, 2));
         console.log('Data saved to transfers.json');
     } catch (error) {
         console.error('Error fetching transfers data:', error);
     }
-})();
+};
+
+trans()
--- index.js
+++ index.js
@@ -11,16 +11,25 @@ const PORT = process.env.PORT || 3000;
 
 app.get('/transfers', async (req, res) => {
     console.log('Fetching transfers data...');
-    try {
-        const data = await transfers(2);
-        
-        res.send(data);
-
-        console.log('Transfers data fetched successfully');
-
-    } catch (error) {
-        res.status(500).send('Error fetching transfers data');
+    const filePath = path.join(__dirname, 'transfers.json');
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error reading data file');
+      return;
     }
+    res.json(JSON.parse(data));
+  });
+    // try {
+    //     const data = await transfers(2);
+        
+    //     res.send(data);
+
+    //     console.log('Transfers data fetched successfully');
+
+    // } catch (error) {
+    //     res.status(500).send('Error fetching transfers data');
+    // }
 });
 
 
