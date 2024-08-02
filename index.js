21b304d9fe5c91213d1c9e1ddfe88d177f602e69
Oluwaseyi <hassanabdullahioluwaseyi@gmail.com>
2024-08-02 12:52:46 +0100

    changes

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 import express from 'express';
-import transfers from './transfers.js'; // Use ES6 import syntax
+import transfers from './transfers.js'; 
+import { topTransfer } from './transfers.js'; 
 import cors from 'cors'
+import path from 'path';
+import fs from 'fs';
+
 
 
 
@@ -9,28 +13,36 @@ const app = express();
 app.use(cors());
 const PORT = process.env.PORT || 3000;
 
+app.get('/', async (req,res) => {
+  res.send('Home')
+})
+
 app.get('/transfers', async (req, res) => {
+  const topTransfers = req.query.topTransfers;
     console.log('Fetching transfers data...');
-    const filePath = path.join(__dirname, 'transfers.json');
-  fs.readFile(filePath, 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-      res.status(500).send('Error reading data file');
-      return;
-    }
-    res.json(JSON.parse(data));
-  });
-    // try {
-    //     const data = await transfers(2);
+
+    // const filePath = path.join(__dirname, '');
+  // fs.readFile('transfers.json', 'utf8', (err, data) => {
+  //   if (err) {
+  //     console.error(err);
+  //     res.status(500).send('Error reading data file');
+  //     return;
+  //   }
+  //   res.json(JSON.parse(data));
+  // });
+
+    try {
+        const data = await (topTransfers ? topTransfer() : transfers(1));
         
-    //     res.send(data);
+        res.send(data);
 
-    //     console.log('Transfers data fetched successfully');
+        console.log('Transfers data fetched successfully');
 
-    // } catch (error) {
-    //     res.status(500).send('Error fetching transfers data');
-    // }
+    } catch (error) {
+        res.status(500).send('Error fetching transfers data');
+    }
 });
+
 
 
 
--- transfers.js
+++ transfers.js
@@ -13,5 +13,21 @@ const transfers = async (pag) => {
     return data
 }
 
+
+const topTransfer = async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(`https://www.fotmob.com/api/transfers?showTop=true`);
+    const data = await page.evaluate(() => {
+        const pre = JSON.parse(document.querySelector('pre').innerText);
+        return pre
+    });
+    await browser.close();
+    return data
+}
+
+
+
 export default transfers;
+export { topTransfer };
 
