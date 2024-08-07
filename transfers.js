98d1df1b1e288b5ea5e87e651fd2f182b30ee6ef
Oluwaseyi <hassanabdullahioluwaseyi@gmail.com>
2024-08-08 00:41:10 +0100

    Some weird changes

--- transfers.js
+++ transfers.js
@@ -1,8 +1,13 @@
 import puppeteer from 'puppeteer';
+require("dotenv").config();
 // const path = require('path');
 
 const transfers = async (pag) => {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+        args: ['--no-sandbox', '--disable-setuid-sandbox', "--single-process", "--no-zygote"],
+        executablePath: 
+            process.env.NODE_ENV === 'production' ? process.env.PUPPETEER_EXECUTABLE_PATH : puppeteer.executablePath(),
+    });
     const page = await browser.newPage();
     await page.goto(`https://www.fotmob.com/api/transfers?&page=${pag}`);
     const data = await page.evaluate(() => {
